b209a658c0a67fda649de8f043fcc708e1d06a21
tyleryouk <tyleryouk@gmail.com>
2023-11-17 12:54:11 -0500

    5:28

--- FundMe.sol
+++ FundMe.sol
@@ -1,42 +1,41 @@
-// get funds from users
-// withdraw funds
-// set a minumum funding value in USD
-
-//SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.22;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
+    using PriceConverter for uint256;
 
-    uint public minUSD = 5;
-
-    function fund() public payable{
-        //want anyone to call fund
-        //how do we send ETH to this contract?
-        //msg.value = number of wei sent to the contract
-        require(msg.value >= minUSD, "didn't send enough eth"); //1e18 = 1ETH
-        //must fund at least 1.0 eth 
-
-    }
-    function getPrice() public {
-        //Address on docs.chain.link price feed addresses 
-        //sepolia testnet eth to usd
-        //0x694AA1769357215DE4FAC081bf1f309aDC325306
-        //ABI
-    }
-    function getConversionRate() public {
-
-    }
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+    address public i_owner; //immutable owner
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
     
-    function getVersion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "need at least $5");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
     }
 
     function withdraw() public {
+        for(uint256 i = 0; i < funders.length; i++){
+            address currentFunder = funders[i]; //setting current funder to a 0xC address
+            addressToAmountFunded[currentFunder] = 0; //setting mapping of 0xC address to 0
+        }
+        funders = new address[](0); //resetting the array funders
+        (bool callSucess, ) = payable(msg.sender).call({value: address(this).balance}(""));
+        require(callSucess,  "Call failed");
+    }
 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "sender is not owner");
+        _;
     }
 }
